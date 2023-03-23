9f6232b6fd40e1e9c41af078352fdb7c76b445fe
Teatek <teatek0512@gmail.com>
2023-03-23 19:22:36 +0100

    Initial commit

---
+++ src/main/java/com/example/ufoodback/UfoodBackApplication.java
@@ -0,0 +1,13 @@
+package com.example.ufoodback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UfoodBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UfoodBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ufoodback/UfoodBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ufoodback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UfoodBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

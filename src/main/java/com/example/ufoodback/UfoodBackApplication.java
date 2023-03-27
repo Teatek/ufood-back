3c5a62d21667c9fa9a08c4c5e90bc87d6ce6ab88
Teatek <teatek0512@gmail.com>
2023-03-27 12:16:27 +0200

    Template package (error and homepage)

--- src/main/java/com/example/ufoodback/UfoodBackApplication.java
+++ src/main/java/com/example/ufoodback/UfoodBackApplication.java
@@ -2,12 +2,22 @@ package com.example.ufoodback;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
+@Controller
 public class UfoodBackApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(UfoodBackApplication.class, args);
+	}
+
+	@GetMapping("/")
+	public String welcome(Model model) {
+		model.addAttribute("title", "Home");
+		return "main";
 	}
 
 }
